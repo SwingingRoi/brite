0b8f8a99ce86c83d9ad5629a0f99617f5a5c8495
SwingingRoi <2259481788@qq.com>
2020-07-09 16:33:55 +0800

    --- Auto-Git Commit ---

--- Java/Export/BriteExportNew.java
+++ Java/Export/BriteExportNew.java
@@ -10,7 +10,37 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+//bacially we dont change the format of file
+ /* 
+ *Export.BriteExport provides functionality to export a topology into
+ * a BRITE format file.  The BRITE format looks like:
+ * <br>
+ *
+ * <pre>
+ * Topology: ( [numNodes] Nodes, [numEdges] Edges )
+ * Model ( [ModelNum] ):  [Model.toString()]
+ *
+ * Nodes: ([numNodes]):
+ * [NodeID]  [x-coord]  [y-coord]  [inDegree] [outDegree] [ASid]  [type]
+ * [NodeID]  [x-coord]  [y-coord]  [inDegree] [outDegree] [ASid]  [type]
+ * [NodeID]  [x-coord]  [y-coord]  [inDegree] [outDegree] [ASid]  [type]
+ * ...
+ *
+ * Edges: ([numEdges]):
+ * [EdgeID]  [fromNodeID]  [toNodeID]  [Length]  [Delay]  [Bandwidth]  [ASFromNodeID]  [ASToNodeID]  [EdgeType]  [Direction]
+ * [EdgeID]  [fromNodeID]  [toNodeID]  [Length]  [Delay]  [Bandwidth]  [ASFromNodeID]  [ASToNodeID]  [EdgeType]  [Direction]
+ * [EdgeID]  [fromNodeID]  [toNodeID]  [Length]  [Delay]  [Bandwidth]  [ASFromNodeID]  [ASToNodeID]  [EdgeType]  [Direction]
+ * ...
+ * </pre>
+ * <br>
+*/
+
 public class BriteExportNew {
-    
-    
+
+    private Topology topology;
+    private BufferedWriter bufferedWriter;
+    private Graph graph;
+    private String modelString;
+
+
 }
